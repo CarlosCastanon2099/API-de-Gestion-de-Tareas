b9d43162ff35be7c402c44d82b65c723906776d8
CarlosCastanon2099 <108638686+CarlosCastanon2099@users.noreply.github.com>
2025-05-07 14:29:16 -0600

    07-05-2025 Swagger y rutas

---
+++ src/routes/taskRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import taskController from "../controllers/taskController.js";
+import { validateCreate, validateUpdate } from "../validators/taskValidator.js";
+
+const router = express.Router();
+
+// Crea la tarea
+outer.post("/tasks", validateCreate, taskController.createTask.bind(taskController));
+// Lista las tareas
+router.get("/tasks", taskController.getTasks.bind(taskController));
+// Obtenemos detalles de la tarea
+router.get("/tasks/:id", taskController.getTaskById.bind(taskController));
+// Actualizamos el estado de la tarea
+router.put("/tasks/:id", validateUpdate, taskController.updateTask.bind(taskController));
+// Eliminamos la tarea
+router.delete("/tasks/:id", taskController.deleteTask.bind(taskController));
+
+export default router;
