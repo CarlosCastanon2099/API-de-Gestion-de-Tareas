a7be8d6a2b06ff7cc44c100a9c1e4973fb8981b2
Mr-Quantum <108638686+CarlosCastanon2099@users.noreply.github.com>
2025-05-09 17:50:32 -0600

    09-05-2025 Tests en Github V.7

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,26 @@
 module.exports = {
   env: {
-    node: true,
-    es2021: true
-  },
-  extends: [
-    "eslint:recommended"
-  ],
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: "module"
+  node: true,
+  es2021: true,
+},
+extends: [
+  'eslint:recommended'
+],
+parserOptions: {
+  ecmaVersion: 2021,
+  sourceType: 'module',
+ecmaFeatures: {
+  importMeta: true
+}},
+rules: {
+  'no-console': 'off',
+  'no-unused-vars': ['warn', { argsIgnorePattern: '^', varsIgnorePattern: '^' }]
+},
+overrides: [{
+  files: ['/tests//*.js'],
+  env: {
+    jest: true
   },
   rules: {
-    "no-console": "off",
-    "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
-  }
-};
+  'no-undef': 'off'
+}}]};
